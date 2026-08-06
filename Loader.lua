dbc7e784a370e698d0b6b2d7196418487da8fa97
Dayvinksthik <daylightgvguv@gmail.com>
2026-08-06 16:50:56 +0800

    ABC

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,16 @@
 local success, result = pcall(function()
-    return game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe"))
+    return game:GetService("HttpService"):JSONDecode(
+        game:HttpGet("https://apis.roblox.com/universes/v1/places/" .. game.PlaceId .. "/universe")
+    )
 end)
 
 local UniverseID = success and result.universeId or nil
 
-if game.PlaceId == 2753915549 or UniverseID == 994732206 then
+if game.PlaceId == 7449423635 or
+   game.PlaceId == 2753915549 or
+   game.PlaceId == 4442272183 or
+   game.PlaceId == 122478697296975 or
+   UniverseID == 994732206 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dayvinksthik/Script/refs/heads/main/BloxFruit.lua"))()
 else
     warn("This game is not supported.")
